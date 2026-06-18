e8282aa3c312138e96857c6b3be7c71682bce971
backdoorkain <51509658+backdoorkain@users.noreply.github.com>
2026-06-18 15:10:14 -0600

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 
-// Configuración de la Base de Datos SQLite
-const db = new sqlite3.Database('/data/torneo.db', (err) => { ... });
+// Configuración de la Base de Datos SQLite (Con ruta persistente para Render)
+const db = new sqlite3.Database('/data/torneo.db', (req, res) => {
+    console.log('Conectado a la base de datos SQLite.');
+});
 
 // Crear tabla adaptada con monto_pago para simulaciones
 db.serialize(() => {
